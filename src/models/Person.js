dd38fceb40256e465655977f5f1f127e70369c7d
rushdina <redredrushrush@gmail.com>
2026-04-10 14:30:00 +0800

    feat: add Person base class with constructor validation, encapsulated properties, and getter methods

---
+++ src/models/Person.js
@@ -0,0 +1,31 @@
+// Parent class
+class Person {
+  constructor(id, name) {
+    if (!id || !name) {
+      throw new Error("Person must have both id and name.");
+    }
+
+    // Internal properties
+    this._id = id;
+    this._name = name;
+  }
+
+  // Getters to access properties
+  get id() {
+    return this._id;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  // Method
+  getDetails() {
+    return {
+      id: this._id,
+      name: this._name,
+    };
+  }
+}
+
+export default Person;
